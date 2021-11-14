75aaaee02678dbafda976cf08a128cafd35bab6e
GKirilov <GKirilov@technologica.com>
2021-11-14 17:05:52 +0200

    Създаване на страница 404

---
+++ front-end/src/app/error/not-foun404/not-foun404.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-not-foun404',
+  templateUrl: './not-foun404.component.html',
+  styleUrls: ['./not-foun404.component.css']
+})
+export class NotFoun404Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
